c5b035318f16f558f220db38b5db6e72946f90e7
Mohd Aqib Khan <uceraqib786@gmail.com>
2022-01-17 18:15:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MovieBuster';
+  select_value:number = 0;
+  value: boolean = true;
+  select(event:any,num:number){
+    if(event.target.innerText=="Unvote"){
+      event.target.innerText = "Vote";
+      this.select_value =0;
+    }
+    else{
+      event.target.innerText = "Unvote";
+      this.select_value = num;
+    }
+    
+  }
+  isSelected(index:number){
+    return this.select_value===index;
+  }
+  anySelected(index:number){
+    if(this.select_value===0){
+      return true;
+    }
+    return this.select_value===index;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDisplayComponent,
+    FlopVotesComponent
   ],
   imports: [
     BrowserModule,
